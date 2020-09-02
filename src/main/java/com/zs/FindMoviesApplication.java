57f2ec4ea642c697d7eabfeb40d5a713451aba9a
zhang-Java <56436571+zhang-Java@users.noreply.github.com>
2020-09-02 20:21:34 +0800

    Update FindMoviesApplication.java

--- src/main/java/com/zs/FindMoviesApplication.java
+++ src/main/java/com/zs/FindMoviesApplication.java
@@ -4,7 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@MapperScan("com.colin.mapper")
+@MapperScan("com.zs.mapper")
 @SpringBootApplication(scanBasePackages = "com.zs")
 public class FindMoviesApplication {
 
